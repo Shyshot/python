388b60a1c8b6d5934fcc951580d02df327f4d113
Shyshot <shyshotno1@gmail.com>
2024-05-12 21:45:05 +0530

    Updated sixth.py

--- Sixth.py
+++ Sixth.py
@@ -18,13 +18,61 @@
 # cal_sum(6,9)
 # cal_sum(7,8)
 
-def cal_avg(a,b,c):
-    sum = a+b+c
-    avg = sum/3
-    print(avg)
+# def cal_avg(a,b,c):
+#     sum = a+b+c
+#     avg = sum/3
+#     print(avg)
 
-a = int(input("Enter first Number"))
-b = int(input("Enter second Number"))
-c = int(input("Enter third Number"))
+# a = int(input("Enter first Number"))
+# b = int(input("Enter second Number"))
+# c = int(input("Enter third Number"))
 
-cal_avg(a,b,c)
+# cal_avg(a,b,c)
+
+#FIXME:Default parameters
+
+'''Default parameters 
+    Assigning a default value of parameter, which is used when no argument is passed
+    '''
+# def pro(a=1,b=3):       #uses a=1 and b=3 as default values when no argument is passed in the function
+#     print(a*b)
+
+# print()
+
+
+list = [1,2,4,5,7,3,8,3,45,78]
+str = "Piyush"
+tup = (1,2,3,5,4,6,3,7,5)
+set = {12,58,57,57,47}
+# def funt(a):
+#     i=0
+#     while i<len(a):
+#         i+=1
+#     print(i)
+
+# funt(list)
+# funt(str)
+# funt(set)
+# funt(tup)
+
+# def display(a):
+#     i=0
+#     while i<len(a):
+#         print(a[i])
+#         i+=1
+
+# display(list)
+# display(str)
+# display(tup)
+
+
+# def fact(a):
+#     i=1
+#     while i<a:
+#         finVal = i*a
+#         i+=1
+#     print(finVal)
+
+# fact(3)
+
+
