a0aca8c11853f0d7d4c217cac52914f7ca7e9548
Shyshot <shyshotno1@gmail.com>
2024-05-12 20:50:09 +0530

    Updated sixthn.py

---
+++ Sixth.py
@@ -0,0 +1,20 @@
+#TODO: Function & Recursion
+
+
+'''Function is block of statements that perform specific task
+    Basic syntax
+    Def fun_name(param1,param2,......):
+        #some work
+        return val
+    
+    fun_name(param1,param2,.....) #function call
+
+'''
+def sum(a,b):
+    sum = a+b
+    print(a+b)
+
+sum(1,2)
+sum(6,9)
+sum(7,8)
+
