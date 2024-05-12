b077f26e6195421886b9c4958717436835f1724b
Shyshot <shyshotno1@gmail.com>
2024-05-12 21:09:28 +0530

    Update sixth.py

--- Sixth.py
+++ Sixth.py
@@ -10,11 +10,21 @@
     fun_name(param1,param2,.....) #function call
 
 '''
-def cal_sum(a,b):
-    sum = a+b
-    print(a+b)
+# def cal_sum(a,b):
+#     sum = a+b
+#     print(a+b)
 
-cal_sum(1,2)
-cal_sum(6,9)
-cal_sum(7,8)
+# cal_sum(1,2)
+# cal_sum(6,9)
+# cal_sum(7,8)
 
+def cal_avg(a,b,c):
+    sum = a+b+c
+    avg = sum/3
+    print(avg)
+
+a = int(input("Enter first Number"))
+b = int(input("Enter second Number"))
+c = int(input("Enter third Number"))
+
+cal_avg(a,b,c)
