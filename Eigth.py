e1c5c95b6ba5f5acad5bc789e69dfcaea6073832
Moscoium <shyshotno1@gmail.com>
2024-05-30 20:38:15 +0530

    Opps

--- Eigth.py
+++ Eigth.py
@@ -26,11 +26,34 @@
 '''
 #Creating class
 class Student:
-    def __init__(self,fullname):
-        self.name =  fullname
+    college_Name = "BHU"
+    name = "Annnoynous"
+    #Default Constructor
+    # def __init__(self) :
+    #     pass
+    #Paramerterized Constructor
+    def __init__(self,name,marks):   #The Can be named Whatever we need, but generally for easier Communication it is kept as self and is used to get variables in the class 
+        self.name =  name
+        self.marks = marks
 
 #Creating Object
-s1 = Student("Karan")
-print(s1.name)
+s1 = Student("Karan",88)
+s2 = Student("Arjun",98)
+
+print(s1.name ,s1.college_Name)
+print(s2.name ,s2.college_Name)
+
 
 #The self parameter is a reference to the current insatance of the class, and is used to access variables that belongs to the class
+#If a constructor has both Default and parameterized construtor then the constructor matching the way the class is called will be Used, Generally Multiple Constructor is not created in a classs
+
+
+
+#FIXME: Class & Instance(Object) Attributes
+
+
+"""Class Attributes are Attributes which are common For all the object For Example: In above class all the objects will have same collge_name Attributes
+   Instance(object) Attributes are attributes which Changes from object to object For Example : In above cass all the objects will have the name and marks Attributes as defined
+"""
+#If Class and object(Instance) both have same Attributes then the Object Attribute will have greater priority
+#This is not usual done but created sometimes to aviod Errors
